1e886552020835fa6bce91724b4b018d3048b8f7
xink <zyhh1611054604@gmail.com>
2026-03-27 23:52:28 +0800

    feat: build cmd

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   entry: ["src/server.ts"],
   format: ["esm"],
   noExternal: [/^@ai-dev-pro\//],
+  banner: {
+    js: `import{createRequire}from'module';const require=createRequire(import.meta.url);`,
+  },
 });
